df8b4909408b52bf0fdab2f1028d014dfc3eb003
samukelolukhele <samukelo.business@gmail.com>
2022-11-15 10:10:01 +0200

    Debugging deployment errors

--- server/src/users/users.module.ts
+++ server/src/users/users.module.ts
@@ -1,13 +1,12 @@
 import { forwardRef, Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { AuthModule } from 'src/auth/auth.module';
 import { UsersController } from './controllers/users.controller';
 import { UsersService } from './services/users.service';
 import { User } from './users.entity';
 
 @Module({
   providers: [UsersService],
-  imports: [TypeOrmModule.forFeature([User]), forwardRef(() => AuthModule)],
+  imports: [TypeOrmModule.forFeature([User])],
   controllers: [UsersController],
   exports: [UsersService],
 })
