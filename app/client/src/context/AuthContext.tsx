978c8c6501548ee9aeb7ac2d04e55717d730d90c
samukelolukhele <samukelo.business@gmail.com>
2022-11-19 16:28:39 +0200

    Debugging CORS: Testing functionality

--- app/client/src/context/AuthContext.tsx
+++ app/client/src/context/AuthContext.tsx
@@ -1,9 +1,9 @@
-import axios from "axios";
-import jwtDecode from "jwt-decode";
-import React, { createContext, useEffect, useState } from "react";
-import { TokenProps } from "../hooks/types/@types.Token";
-import { User } from "../hooks/types/@types.User";
-import useFetch from "../hooks/useFetch";
+import axios from 'axios';
+import jwtDecode from 'jwt-decode';
+import React, { createContext, useEffect, useState } from 'react';
+import { TokenProps } from '../hooks/types/@types.Token';
+import { User } from '../hooks/types/@types.User';
+import useFetch from '../hooks/useFetch';
 
 interface AProps {
   children?: React.ReactNode;
@@ -14,25 +14,25 @@ export const AuthContext = createContext<AProps>({});
 
 const AuthContextProvider = ({ children }: { children: React.ReactNode }) => {
   const [loggedInUser, setLoggedInUser] = useState<User>({
-    email: "",
-    username: "",
+    email: '',
+    username: '',
     id: null,
-    description: "",
-    profile_image: "",
-    full_name: "",
+    description: '',
+    profile_image: '',
+    full_name: '',
     note: [
       {
-        id: "",
-        title: "",
-        content: "",
+        id: '',
+        title: '',
+        content: '',
         is_pinned: false,
-        authorId: "",
-        created_at: "",
-        updated_at: "",
+        authorId: '',
+        created_at: '',
+        updated_at: '',
       },
     ],
   });
-  const token = localStorage.getItem("token");
+  const token = localStorage.getItem('token');
 
   const checkExpiration = async (token: any) => {
     if (!token) return;
@@ -41,23 +41,22 @@ const AuthContextProvider = ({ children }: { children: React.ReactNode }) => {
 
     if (decodedToken.exp * 1000 < Date.now()) {
       window.location.reload();
-      return localStorage.removeItem("token");
+      return localStorage.removeItem('token');
     }
   };
 
   const handleLoggedInUser = async () => {
     axios
-      .get("users/profile", {
+      .get('users/profile', {
         headers: {
-          Authorization: `Bearer ${localStorage.getItem("token")}`,
+          Authorization: `Bearer ${localStorage.getItem('token')}`,
         },
       })
       .then((res) => setLoggedInUser(res.data))
       .catch((err) => {
-        console.log(err);
         if (err.response.status != 500 || 408 || 401) return;
         window.location.reload();
-        return localStorage.removeItem("token");
+        return localStorage.removeItem('token');
       });
   };
 
--- app/src/main.ts
+++ app/src/main.ts
@@ -5,9 +5,20 @@ import { AppModule } from './app.module';
 import { join } from 'path';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { cors: true });
+  const app = await NestFactory.create(AppModule);
+
+  app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PATCH,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Accept');
+    next();
+  });
+
   app.setGlobalPrefix('api');
-  app.enableCors();
+  app.enableCors({
+    allowedHeaders: '*',
+    origin: '*',
+  });
   await app.listen(8080);
 }
 bootstrap();
