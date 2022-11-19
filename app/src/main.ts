8df9fe8378e54f701a5f3118e3ec8c1a29137de0
samukelolukhele <samukelo.business@gmail.com>
2022-11-19 17:23:12 +0200

    Debugging CORS: Testing functionality.

--- app/src/main.ts
+++ app/src/main.ts
@@ -7,18 +7,11 @@ import { join } from 'path';
 export default async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Methods', 'GET,PATCH,POST,DELETE');
-    res.header('Access-Control-Allow-Headers', 'Content-Type, Accept');
-    next();
-  });
-
   app.setGlobalPrefix('api');
   app.enableCors({
     allowedHeaders: '*',
     origin: '*',
   });
-  // await app.listen(8080);
+  await app.listen(8080);
 }
 bootstrap();
